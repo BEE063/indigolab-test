2ffd77dd02abc8efa0e07b726d98f2a6821ab7d7
eantonova <antonova.hellokitty2011@yandex.ru>
2025-03-19 18:01:08 +0300

    add project service

---
+++ src/app/shared/services/project.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { ProjectDTO } from '../../models/project.dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+  getProjects(): ProjectDTO[] {
+    return [
+      {
+        "id": 1,
+        "name": "Проект A",
+        "tasksCompleted": 25,
+        "tasksTotal": 100,
+        "startDate": "2024-01-01",
+        "endDate": "2024-12-31"
+        },
+        {
+        "id": 2,
+        "name": "Проект B",
+        "tasksCompleted": 75,
+        "tasksTotal": 140,
+        "startDate": "2023-06-01",
+        "endDate": "2024-03-31"
+        },
+        {
+        "id": 3,
+        "name": "Проект C",
+        "tasksCompleted": 80,
+        "tasksTotal": 85,
+        "startDate": "2024-06-01",
+        "endDate": "2024-09-30"
+        }
+    ]
+  }
+}
