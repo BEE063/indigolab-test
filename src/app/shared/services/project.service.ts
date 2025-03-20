d306860074b686b903806b2fd36b3a6512602aac
eantonova <antonova.hellokitty2011@yandex.ru>
2025-03-20 14:01:39 +0300

    add setProjects()

--- src/app/shared/services/project.service.ts
+++ src/app/shared/services/project.service.ts
@@ -2,35 +2,43 @@ import { Injectable } from '@angular/core';
 import { ProjectDTO } from '../../models/project.dto';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProjectService {
   getProjects(): ProjectDTO[] {
-    return [
-      {
-        "id": 1,
-        "name": "Проект A",
-        "tasksCompleted": 25,
-        "tasksTotal": 100,
-        "startDate": "2024-01-01",
-        "endDate": "2024-12-31"
+    if (localStorage.getItem('projects')) {
+      return JSON.parse(localStorage.getItem('projects') ?? '');
+    } else {
+      return [
+        {
+          id: 1,
+          name: 'Проект A',
+          tasksCompleted: 25,
+          tasksTotal: 100,
+          startDate: '2024-01-01',
+          endDate: '2024-12-31',
         },
         {
-        "id": 2,
-        "name": "Проект B",
-        "tasksCompleted": 75,
-        "tasksTotal": 140,
-        "startDate": "2023-06-01",
-        "endDate": "2024-03-31"
+          id: 2,
+          name: 'Проект B',
+          tasksCompleted: 75,
+          tasksTotal: 140,
+          startDate: '2023-06-01',
+          endDate: '2024-03-31',
         },
         {
-        "id": 3,
-        "name": "Проект C",
-        "tasksCompleted": 80,
-        "tasksTotal": 85,
-        "startDate": "2024-06-01",
-        "endDate": "2024-09-30"
-        }
-    ]
+          id: 3,
+          name: 'Проект C',
+          tasksCompleted: 80,
+          tasksTotal: 85,
+          startDate: '2024-06-01',
+          endDate: '2024-09-30',
+        },
+      ];
+    }
+  }
+
+  setProjects(projects: ProjectDTO[]): void {
+    localStorage.setItem('projects', JSON.stringify(projects));
   }
 }
